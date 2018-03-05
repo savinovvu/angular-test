e7f4e87a611de74315ed19355da6d305e71c99f8
v.savinov <v.savinov@irkpk.ru>
2018-03-05 14:08:01 +0800

    all test passed.

--- e2e/members.e2e-spec.ts
+++ e2e/members.e2e-spec.ts
@@ -2,20 +2,28 @@ import {browser, by, element} from 'protractor';
 
 describe('Given should test the search feature', () => {
   let searchBox, searchButton, searchResult;
+
   beforeEach(() => {
     browser.get('');
-    element(by.linkText('Search')).click();
-    searchResult = element.all(by.css('#searchList tbody tr'));
-    expect(searchResult.count()).toBe(3);
-
+    element(by.linkText('members')).click();
     searchButton = element(by.css('form button'));
     searchBox = element(by.css('form input'));
-    searchBox.sendKeys('Thomas');
-    searchButton.click();
+
+  });
+
+  it('should have an input and search button', () => {
+    expect(searchButton.isPresent()).toEqual(true);
+    expect(searchButton.isPresent()).toEqual(true);
+
+    searchResult = element.all(by.css('#searchList tbody tr'));
+    expect(searchResult.count()).toBe(3);
   });
 
   it('There should be one item in search result', () => {
-    searchResult = element.all(by.css('#searchList tbody tr'));
-    expect(searhResult.count()).toBe(1);
+    searchBox.sendKeys('Thomas');
+    searchButton.click().then(() => {
+      searchResult = element.all(by.css('#searchList tbody tr'));
+      expect(searchResult.count()).toBe(1);
+    });
   });
 });
